b050b9785210bd0c3ca0d52bbd7fc36f19e8d666
Philippe THOMY <philippe@loco-labs.io>
2024-10-23 08:58:20 +0200

    version initiale
    commit e3ebc17fa3d30c65184949f6858c9faf369b5bb9
    Author: Philippe THOMY <philippe@loco-labs.io>
    Date:   Wed Oct 23 08:56:53 2024 +0200
    
        Update exemple_afir_V5.ipynb
    
    commit 6643846fb7ad45e7e51bae2fc74edd3074a5e23f
    Author: Philippe THOMY <philippe@loco-labs.io>
    Date:   Wed Oct 23 00:01:14 2024 +0200
    
        asfa
    
    commit 5d00a2c65aef6de3f941071391a59825b1ae1a7c
    Author: Philippe THOMY <philippe@loco-labs.io>
    Date:   Tue Oct 22 12:57:23 2024 +0200
    
        asfa
    
    commit 87a8eed55d7317f0f44e377cdca64d68308b62d8
    Author: Philippe THOMY <philippe@loco-labs.io>
    Date:   Tue Oct 22 00:49:32 2024 +0200
    
        asfa tests
    
    commit 9fb5fe0efdf39780c71916c0399cac8787cf5f10
    Author: Philippe THOMY <philippe@loco-labs.io>
    Date:   Mon Oct 21 21:38:40 2024 +0200
    
        Update V1_afir_rtet_asfa.ipynb
    
    commit 6a32f0a2dbada7cab1cfbb5f10c05e944b92985f
    Author: Philippe THOMY <philippe@loco-labs.io>
    Date:   Mon Oct 21 19:28:43 2024 +0200
    
        Create V1_afir_rtet_asfa.ipynb
    
    commit 3ffd683d8ff2ff3b44687fc9d834da2823a9f2ef
    Author: Philippe THOMY <philippe@loco-labs.io>
    Date:   Mon Oct 21 15:39:30 2024 +0200
    
        ajout noms
    
    commit d63fc0a70ff2eeb1e89229d40c2db6a065adec48
    Author: Philippe THOMY <philippe@loco-labs.io>
    Date:   Mon Oct 21 12:25:56 2024 +0200
    
        separation rtet
    
    commit 5cc6350899b7fb3edfb629ebc43e3c5a848dacd7
    Author: Philippe THOMY <philippe@loco-labs.io>
    Date:   Mon Oct 21 01:50:35 2024 +0200
    
        afir_v5 et id_node integer
    
    commit 80bfa265e6a098f503f71b5182150c61e733e409
    Author: Philippe THOMY <philippe@loco-labs.io>
    Date:   Sun Oct 20 00:34:51 2024 +0200
    
        saturation étendue
    
    commit 338e96e893192340a69e21345e58afbc36dd666c
    Author: Philippe THOMY <philippe@loco-labs.io>
    Date:   Sat Oct 19 01:28:53 2024 +0200
    
        stockage réseau
    
    commit d424500da3fd759d68459e87a95a5f4c39531227
    Author: Philippe THOMY <philippe@loco-labs.io>
    Date:   Fri Oct 18 18:56:42 2024 +0200
    
        reorg fichiers
    
    commit 845a204da483f09dda741091f6d761d96664848f
    Author: Philippe THOMY <philippe@loco-labs.io>
    Date:   Fri Oct 18 11:27:31 2024 +0200
    
        reorg fichiers
    
    commit e251069033c0885606cd267de60a30f1016533b8
    Author: Philippe THOMY <philippe@loco-labs.io>
    Date:   Fri Oct 18 10:51:11 2024 +0200
    
        migration

---
+++ notebooks/fonction_rtet.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Ce module contient les fonctions utilisées dans l'analyse du réseau RTET des infrastructure IRVE.
+"""
+import math
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+import networkx as nx
+import geo_nx as gnx
+from geo_nx import cast_id
+
+def insertion_noeuds(noeuds, gr, proxi, att_node=None, troncons=None, adjust=False):
+    '''insere des 'noeuds' sur le graph 'gr' pour des troncons définis et retourne le graphe des 'noeuds' '''
+    troncons = troncons if troncons is not None else gr.to_geopandas_edgelist()
+    join = gpd.sjoin(noeuds, troncons.set_geometry(troncons.buffer(proxi))) # filtrage des tronçons
+    noeuds_ok = noeuds[noeuds.index.isin(join.index)].copy()
+    noeuds_ok['node_id'] = range(max(gr.nodes)+1, len(noeuds_ok)+max(gr.nodes)+1)
+    gs_noeuds = gnx.from_geopandas_nodelist(noeuds_ok, node_id='node_id', node_attr=True) # réseau des noeuds supplémentaires
+    # à vectoriser
+    for noeud in gs_noeuds:
+        geo_st = gs_noeuds.nodes[noeud]['geometry']
+        id_edge = gr.find_nearest_edge(geo_st, proxi) # recherche du troncon le plus proche
+        #id_node = int(max(cast_id(gr.nodes, only_int=True)) + 1)
+        #id_node = gs_noeuds.nodes[noeud]['node_id']
+        att_node = att_node if att_node is not None else {key: val for key, val in gs_noeuds.nodes[noeud].items() if key not in ['node_id', 'geometry']}
+        gr.insert_node(geo_st, noeud, id_edge, att_node=att_node, adjust=adjust) # ajout du noeud
+    return gs_noeuds
+
+def proximite(noeuds_ext, cible, proxi):
+    '''sépare les noeuds proches (distance < proxi) et non proches''' 
+    st_join = gpd.sjoin(noeuds_ext, cible.set_geometry(cible.buffer(proxi)))
+    st_ok = noeuds_ext.index.isin(st_join.index)
+    return (noeuds_ext[st_ok], noeuds_ext[~st_ok])
+
+def insertion_projection(nodes_ext, node_attr, edge_attr, gr, proxi, att_insert_node):
+    '''création du graphe des stations avec projection sur des noeuds inserés dans le graphe 'gr' '''
+    gr_ext = gnx.from_geopandas_nodelist(nodes_ext, node_id='node_id', node_attr=node_attr)
+    stations = list(nodes_ext['node_id'])
+    st_ko =[]
+    for station in stations: # à vectoriser
+        dist = gr_ext.project_node(station, gr, proxi, att_edge=edge_attr)
+        if not dist:
+            geo_st = gr_ext.nodes[station]['geometry']
+            id_edge = gr.find_nearest_edge(geo_st, proxi)
+            if not id_edge: 
+                st_ko.append(station)
+                print('ko')
+                continue 
+            # on ajoute un noeud et on crée le lien 
+            id_node = max(max(gr.nodes) + 1, max(gr_ext.nodes) + 1)
+            # print(id_node)
+            dis = gr.insert_node(geo_st, id_node, id_edge, att_node=att_insert_node, adjust=False) 
+            if not dis: # cas à clarifier  
+                st_ko.append(station)
+                print(id_node, id_edge, geo_st)
+                continue
+            gr_ext.project_node(station, gr, 0, target_node=id_node, att_edge=edge_attr)
+    return gr_ext, st_ko
+
+def troncons_non_mailles(g_tot, gr_ext, gr, dispo, seuil): 
+    '''identifie les tronçons qui ont au moins un point à une distance supérieure à 'seuil' de la plus proche station non saturée''' 
+    saturation = []
+    #gr_stat_satur = nx.subgraph_view(gr, filter_node=(lambda x: not gr.nodes[x].get(dispo, True)))
+    gr_stat_satur = nx.induced_subgraph(gr_ext, [nd for nd in gr_ext.nodes if not gr_ext.nodes[nd].get(dispo, True)])
+    #gr_ext_st = nx.subgraph_view(gr_ext, filter_node=(lambda x: isinstance(x, str) and x[:2] == 'st'))
+    gr_ext_st = nx.subgraph_view(gr_ext, filter_node=(lambda x: 'nature' in gr_ext.nodes[x] and gr_ext.nodes[x]['nature'] == 'station_irve'))
+    for edge in gr.edges:
+        dist_inter_st = g_tot.weight_extend(edge, gr_ext_st, radius=seuil, n_attribute='dist_node_ext', n_active=dispo)
+        #dist_inter_st = gr.weight_extend(edge, gr_ext_st, radius=seuil, n_attribute='dist_node_ext', n_active=dispo)
+        if not dist_inter_st or dist_inter_st > 2 * seuil :
+            saturation.append(edge)
+    # gr_satur = nx.subgraph_view(gr, filter_edge=(lambda x1, x2: (x1, x2) in saturation))
+    gr_satur = gr.edge_subgraph(saturation)
+    return gr_stat_satur, gr_satur
+
+def troncons_peu_mailles(gr_satur, g_tot, dispo):
+    '''complète gr_satur avec les tronçons allant jusqu'à une bifurcation ou une station disponible'''
+    nd_sat = set(gr_satur.nodes())
+    ed_extend = set(gr_satur.edges())
+    for node in gr_satur.nodes():
+        adjs = set(nd for nd in g_tot.adj[node] 
+                   if nd not in nd_sat and (dispo not in g_tot.nodes[nd] or g_tot.nodes[nd][dispo])
+                  ) - nd_sat
+        #print('adjs init ', adjs)
+        while len(adjs) == 1:
+            nd_sat.add(node)
+            new_node = list(adjs)[0]
+            ed_extend.add((node, new_node))
+            node = new_node
+            adjs = set(nd for nd in g_tot.adj[node] 
+                       if nd not in nd_sat and (dispo not in g_tot.nodes[nd] or g_tot.nodes[nd][dispo])
+                      ) - nd_sat
+    return g_tot.edge_subgraph(ed_extend)
